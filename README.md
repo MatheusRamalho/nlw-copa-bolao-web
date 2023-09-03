cdfd4cd77a4c68b751a0af91000bdc2324adab05
Matheus Ramalho Silva <ramalho.dev@gmail.com>
2023-09-03 13:52:59 -0300

    refactor(main): add eslint, readme and update dependencies

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center"> NLW #10 Copa bolão </h1>
+
+<p align="center">
+Evento exclusivo e gratuito, promovido pela Rocketseat para ensino de tecnologias WEB.
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="calendario da copa" src=".github/preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- React JS
+- Next JS
+- TypeScript
+- Tailwindcss
+- Git e Github
+
+## 💻 Projeto
+
+Um bolã da Copa é um projeto para criar um bolão com os amigos sobre os jogos da Copa de 2022.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/J1Z33MISC22YZB8wfxiIns/NLW-Copa-Explorer/duplicate). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Projeto proposto pela Rocketseat no NLW 10 copa do mundo 2022. <br/>
+Feito com ♥ by Matheus Ramalho :wave: [matheusramalho.dev](matheusramalho.dev)
---
+++ src/app/page.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { FormEvent, useState } from 'react'
+import Image from 'next/image'
+
+import { api } from '../libs/axios'
+
+import logoImg from '../assets/logo.svg'
+import avatarsImg from '../assets/avatars.png'
+import previewImg from '../assets/preview.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+interface HomeProps {
+    pollCount: number
+    guessCount: number
+    userCount: number
+}
+
+export default function Home({ pollCount, guessCount, userCount }: HomeProps) {
+    const [pollTitle, setPollTitle] = useState<string>('')
+
+    const createPoll = async (event: FormEvent) => {
+        event.preventDefault()
+
+        try {
+            const response = await api.post('/polls', { title: pollTitle })
+            const { code } = response.data
+
+            await navigator.clipboard.writeText(code) // Copia para area de transferencia do usuario...
+
+            alert('Bolão criado com sucesso, o código foi copiado para área de transferência!')
+            setPollTitle('')
+        } catch (error) {
+            alert('Falha ao criar o bolão, tente novamente!')
+        }
+    }
+
+    return (
+        <div className="container min-h-screen mx-auto p-8 flex items-center justify-center gap-28 flex-col lg:flex-row">
+            <main>
+                <Image src={logoImg} alt="NLW Copa" />
+
+                <h1 className="mt-14 font-bold leading-tight text-white text-3xl sm:text-5xl">
+                    Crie seu próprio bolão da copa e compartilhe com amigos!
+                </h1>
+
+                <div className="mt-10 flex items-center gap-2">
+                    <Image src={avatarsImg} alt="" />
+
+                    <strong className="text-gray-100 text-xl">
+                        <span className="text-green-500"> +{userCount} </span>
+                        pessoas já estão usando
+                    </strong>
+                </div>
+
+                <form onSubmit={createPoll} className="mt-10 flex gap-2 flex-col sm:flex-row">
+                    <input
+                        type="text"
+                        required
+                        placeholder="Qual o nome do seu bolão?"
+                        className="flex-1 px-6 py-4 rounded border border-gray-600 bg-gray-800 text-sm text-gray-100"
+                        value={pollTitle}
+                        onChange={(event) => setPollTitle(event.target.value)}
+                    />
+
+                    <button
+                        type="submit"
+                        className="px-6 py-4 rounded bg-yellow-500 font-bold uppercase text-sm text-gray-900 hover:bg-yellow-700"
+                    >
+                        Criar meu bolão
+                    </button>
+                </form>
+
+                <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+                    Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+                </p>
+
+                <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-start gap-10 flex-col sm:flex-row text-gray-100">
+                    <div className="flex items-center justify-center sm:justify-start gap-6">
+                        <Image src={iconCheckImg} alt="" />
+
+                        <div className="flex flex-col">
+                            <span className="font-bold text-2xl"> +{pollCount} </span>
+                            <span> Bolões criados </span>
+                        </div>
+                    </div>
+
+                    <div className="w-px h-14 bg-gray-600 hidden sm:block"></div>
+
+                    <div className="flex items-center justify-center sm:justify-start gap-6">
+                        <Image src={iconCheckImg} alt="" />
+
+                        <div className="flex flex-col">
+                            <span className="font-bold text-2xl"> +{guessCount} </span>
+                            <span> Palpites enviador </span>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <Image quality={100} src={previewImg} alt="Dois celulares exibindo uma prévia da aplicação móvel da NLW Copa" />
+        </div>
+    )
+}
